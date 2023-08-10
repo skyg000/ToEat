2c72378ebd14f05f3b83870d53d6eb4cfa7e1834
hsk <skyg00700@gmail.com>
2023-08-10 18:17:36 +0900

    j

--- toeat/js/sub2.js
+++ toeat/js/sub2.js
@@ -1,3 +1,54 @@
+let s_code = ['1721530874','11717437'], w_code = ['31564605','13321644'];
+
+sessionStorage.setItem('s_code',s_code)
+localStorage.setItem('w_code',w_code)
+
+
+const con = document.querySelector('.con')
+let tag ='', s_arr = sessionStorage.s_code.split(','), l_arr = localStorage.w_code.split(',');
+
+$(document).ready(function(){
+    $(".list ul li a").each(function(k,v){
+        $(this).click(function(e){
+            e.preventDefault();
+            con.innerHTML=``;
+            $(".list ul li").removeClass('on')
+            $(".list ul li").eq(k).addClass('on')
+            if(v.innerText == "최근 본 맛집"){
+                tag ='';
+                list(s_arr)
+            } else {
+                tag ='';
+                list(l_arr)
+            }
+        })
+    })
+
+})
+
+function list(q) {
+    fetch('./js/data/md.json')
+    .then(res=>res.json())
+    .then(data=>{
+        data.forEach((v,k)=>{
+            for(let i=0; i<q.length; i++){
+                if(v.code == q[i]){
+                    tag += `
+                    <li class="on2">
+                        <img src="${v.images}" alt="">
+                        <p>${v.name}</p>
+                    </li>
+                    `
+                }
+            }
+        })
+        con.innerHTML = tag;
+    })
+}
+
+list(s_arr)
+
+
 
 /* function con (){
     const ele = document.querySelectorAll('.test li ')
@@ -10,15 +61,19 @@
         })
 } */
 
-$(document).ready(function(){
 
 
-    $(".list ul li a").each(function(k,v){
-        $(this).click(function(e){
-            e.preventDefault();
-            $(".list ul li").removeClass('on')
-            $(".list ul li").eq(k).addClass('on')
-        })
-    })
-
+/* fetch('./js/data/md.json')
+.then(res=>res.json())
+.then(data=>{
+    const stores = document.querySelector('.stores')
+    data.data.forEach(obj => {
+        stores.innerHTML += obj.code;
+    });
 })
+
+
+$(".store").click(function(e){
+    e.preventDefault();
+
+}) */
